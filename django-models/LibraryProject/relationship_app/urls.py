fbad69f7c864c4627bfd54b2e9036b71b41766c3
Vincevinty <vincevinty@gmail.com>
2025-09-08 00:06:25 +0200

    added login,logout, and register routes to urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import home_view
 from .views import list_books, LibraryDetailView, login_view, logout_view, register_view
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
