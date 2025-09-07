5d5be54d78bd9160eb442c0ac95550c3076901a8
Vincevinty <vincevinty@gmail.com>
2025-09-07 22:26:06 +0200

    Wrote a script that implements a function-based view and a class-based view

---
+++ django-models/LibraryProject/relationship_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Book, Library
+from django.views.generic.detail import DetailView
+
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# Class-based view to show book details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
