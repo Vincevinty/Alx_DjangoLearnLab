d123fcb889804db96e76515cb95e5f3827fd07f8
Vincevinty <vincevinty@gmail.com>
2025-09-08 01:07:59 +0200

    Added URL patterns for Book views: list, create, update, delete

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,6 +4,8 @@ from .views import list_books, LibraryDetailView, login_view, logout_view, regis
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import admin_view, librarian_view, member_view
+from .views import add_book, edit_book, delete_book
+
 
 
 
@@ -17,4 +19,7 @@ urlpatterns = [
     path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
+    path('books/add/', add_book, name='add_book'),
+    path('books/edit/<int:pk>/', edit_book, name='edit_book'),
+    path('books/delete/<int:pk>/', delete_book, name='delete_book'),
 ]
