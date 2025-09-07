921918fc894ff6e9682f976f865e1b5fbcc86d22
Vincevinty <vincevinty@gmail.com>
2025-09-08 00:55:10 +0200

    Added custom permissions to Book model for add, change, and delete actions

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -41,3 +41,19 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
+    
+    class Book(models.Model):
+     title = models.CharField(max_length=255)
+     author = models.CharField(max_length=255)
+     published_date = models.DateField()
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+        
+        def __str__(self):
+            return self.title    
+
