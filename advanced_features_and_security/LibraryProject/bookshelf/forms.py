83c6a49906258bf0b28e26c3ddc87ebe39162983
Vincevinty <vincevinty@gmail.com>
2025-09-14 09:43:20 +0200

    added ExampleForms on my script

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from .models import Book
 
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
+
 class BookSearchForm(forms.Form):
     query = forms.CharField(
         max_length=100,
