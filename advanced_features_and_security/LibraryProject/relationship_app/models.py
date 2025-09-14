5ce88371520ee2932dc162972c8950be3f522556
Vincevinty <vincevinty@gmail.com>
2025-09-14 07:49:24 +0200

    Added import statement to get user model

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 
+user = get_user_model()
+
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
