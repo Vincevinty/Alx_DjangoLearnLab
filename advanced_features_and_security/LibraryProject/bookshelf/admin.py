558ea4a71ee7b7d4ad6c6c33724f5b46d5c7eb2d
Vincevinty <vincevinty@gmail.com>
2025-09-14 07:37:29 +0200

    Intergrate the Custom User Model Into Admin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Book
+from .models import CustomUser
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')  # Columns shown in admin list view
     search_fields = ('title', 'author')                     # Enables search box
     list_filter = ('publication_year',)                     # Adds sidebar filter
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
