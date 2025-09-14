23e7ea093737441407471e4b59d7c775d3ee1f06
Vincevinty <vincevinty@gmail.com>
2025-09-14 09:25:50 +0200

    A script that prevents SQL injection and ensures inputs are validated before querying the database.

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Book
 from django.contrib.auth.decorators import permission_required
+from django import forms
 
 @permission_required('bookshelf.view_book', raise_exception=True)
 
@@ -9,6 +10,9 @@ def book_list(request):
     query = request.GET.get('q', '')
     books = Book.objects.filter(title__icontains=query)
     return render(request, 'bookshelf/book_list.html', {'books': books})
+
+class BookSearchForm(forms.Form):
+    q = forms.CharField(max_length=100)
 
 
 
