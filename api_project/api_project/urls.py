accc88af36d728863b525abd7321a3def01ca77f
Vincevinty <vincevinty@gmail.com>
2025-09-23 21:57:43 +0200

    Setting up corresponding url

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .views import BookList
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/books/', BookList.as_view(), name='book-list'),
+    path('api/', include('books.urls')),
+    path('admin/', admin.site.urls),
+     path('api/', include('api.urls')),
 ]
