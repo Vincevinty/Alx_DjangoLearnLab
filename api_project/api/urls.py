47806b7459dc917e377b773704405827d6c3d88f
Vincevinty <vincevinty@gmail.com>
2025-09-24 13:30:20 +0200

    Fixed urls module

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
 
 router = DefaultRouter()
-router.register(r'books_all', BookViewSet, basename='book_all')
+router.register(r'books_all', BookViewSet, basename='books_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
