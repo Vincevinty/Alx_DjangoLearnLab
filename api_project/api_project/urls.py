f356664a414be74335b0c4824be1645022d21d21
Vincevinty <vincevinty@gmail.com>
2025-09-24 13:36:05 +0200

    Fixed identation on line 24 for my urlspattern

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -21,5 +21,5 @@ from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('books.urls')),
-     path('api/', include('api.urls')),
+    path('api/', include('api.urls')),
 ]
