50c07d0a14ef03c4cf059bfc6611ddd693123d31
Vincevinty <vincevinty@gmail.com>
2025-10-03 21:26:27 +0200

    Update Book model to include related_name='books' on Author ForeignKey

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -9,7 +9,7 @@ class Author(models.Model): # Defines the Author model
 class Book(models.Model): # Defines the Book model
     title = models.CharField(max_length = 200) # Stores the book's title
     publication_year = models.IntegerField() # Stores the publication year of the book
-    author = models.ForeignKey(Author, on_delete = models.CASCADE) # Links to the Author model
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE) # Foreign key relationship to Author model
 
     def __str__(self): # String representation of the Book model
         return self.title # Returns the book's title
