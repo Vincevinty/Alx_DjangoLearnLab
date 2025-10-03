2e8a32b58a63d9eaaeea50ea6efe50754e2f479a
Vincevinty <125501807+Vincevinty@users.noreply.github.com>
2025-10-03 19:08:58 +0200

    Delete advanced-api-project/advanced_api_project directory

--- advanced-api-project/advanced_api_project/api/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Author, Book
-
-admin.site.register(Author) # Registers the Author model with the admin site
-admin.site.register(Book)   # Registers the Book model with the admin site
