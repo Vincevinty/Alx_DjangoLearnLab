64aa6bb98dddc4c352758465808bc176936bb2e4
Vincevinty <vincevinty@gmail.com>
2025-10-04 02:31:36 +0200

    Feat(Routing): Implement named list and detail routes for Book API

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -15,4 +15,8 @@ urlpatterns = [
     path('books/update/', BookUpdateView.as_view()), # Added URL pattern for updating a book
     path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # Added URL pattern for deleting a book
     path('books/delete/', BookDeleteView.as_view()), # Added URL pattern for deleting a book
+    # Endpoint for GET (List) and POST (Create)
+    # The name='book-list' is used for reverse URL matching in tests and elsewhere
+    path('books/', BookListCreateView.as_view(), name='book-list'), 
+    path('books/<int:pk>/', BookRetrieveUpdateDestroyView.as_view(), name='book-detail'),
 ]
