3b7638fc106729e57dfb592a4a6e192a9603ac99
Vincevinty <vincevinty@gmail.com>
2025-10-05 22:25:09 +0200

    Fixed: ForeignKey letter K which i wrote as a small letter

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -6,6 +6,6 @@ class Post(models.Model):
     title = models.CharField(max_length = 200)
     content = models.TextField()
     published_date = models,DateField(auto_now_add=True)
-    author = models.Foreignkey(User, on_delete = models.CASCADE, related_name = 'posts')
+    author = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'posts')
 
 
