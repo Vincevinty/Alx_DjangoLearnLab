cc1e81c19c30cba271e276efda8d806b9a5fe32c
Vincevinty <vincevinty@gmail.com>
2025-10-10 00:04:53 +0200

    Configured authentication URL patterns for login, logout, register, and profile

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import auth_views
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', views.register, name='register'), # User registration view
+    path('profile/', views.profile, name='profile'), # User profile view
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'), # Custom login view
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'), # Redirect to login page after logout
+    path('', include('blog.urls')),  # Include blog app URLs
 ]
