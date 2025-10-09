a4fd2dae58be0f2afc4d54cc78f2fc1fdff998eb
Vincevinty <vincevinty@gmail.com>
2025-10-09 22:48:45 +0200

    Added authentication URL paths for register, profile, login, and logout

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,10 @@
-from django.contrib.auth import auth_views
 from django.urls import path
+from django.contrib.auth import auth_views
+from . import views
 
-path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+]
