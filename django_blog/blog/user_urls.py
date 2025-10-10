c12ba448447cd280a077510e0c1582fca58e810b
Vincevinty <vincevinty@gmail.com>
2025-10-10 07:11:22 +0200

    Added user to handle authentication routes seperately

---
+++ django_blog/blog/user_urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views 
+from django.contrib.auth import views as auth_views
+
+# This file handles all URLs prefixed with 'users/' 
+urlpatterns = [
+    # Custom views defined in blog/views.py
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    
+    # Built-in Django authentication views (Login/Logout)
+    # The 'login' name is used by default by Django's auth system for redirects
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    
+    # After logging out, the user is redirected back to the login page
+    path('logout/', auth_views.LogoutView.as_view(next_page='/users/login/'), name='logout'),
+]
