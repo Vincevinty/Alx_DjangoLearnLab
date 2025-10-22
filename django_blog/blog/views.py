f4a362174466670cf2c872b43f6a5bbf77580fa7
Vincevinty <vincevinty@gmail.com>
2025-10-22 21:36:35 +0200

    Added profile view for authenticated users with email update

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 def register_view(request):
     if request.method == 'POST':
@@ -10,3 +12,10 @@ def register_view(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile_view(request):
+    if request.method == 'POST':
+        request.user.email = request.POST.get('email')
+        request.user.save()
+    return render(request, 'blog/profile.html', {'user': request.user})
